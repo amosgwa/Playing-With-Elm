3f926e4ac33dfd4b8d37aa9a3dcac08d337ead3f
Amos Gwa <amosgwa@gmail.com>
2016-12-05 09:28:36 -0700

    Started from the bottom

---
+++ Tutorial/Payload.elm
@@ -0,0 +1,67 @@
+module Payload exposing (..)
+
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = Increment Int
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [onClick (Increment 2)] [ text "+" ]
+        , text (toString model)
+        ]
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment howmuch -> 
+            (model + howmuch, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
